2603a1cf0a4fa10541989ac0341804c5abdb1267
Yishai Gronich <yishai.gronich@gmail.com>
2016-04-27 21:15:26 +0300

    [~] Working detector
    [+] Option to save the detector to memory
    [+] ROC curve, does not work well yet.

--- BagOfWordsDetector/__main__.py
+++ BagOfWordsDetector/__main__.py
@@ -1,31 +1,94 @@
+import pickle
+
+import numpy as np
+import matplotlib.pyplot as plt
+
 import cv2
 import detector
 
-motorcycle_detector = detector.ObjectDetector()
-motorcycle_detector.train('./img/neg', './img/pos')
 
-print('\nMagical Predictions:')
-test_img = cv2.imread('./img/pos/motorcycle001.jpg')
-print(motorcycle_detector.predict(test_img))
-test_img = cv2.imread('./img/pos/motorcycle005.jpg')
-print(motorcycle_detector.predict(test_img))
-test_img = cv2.imread('./img/pos/motorcycle010.jpg')
-print(motorcycle_detector.predict(test_img))
-test_img = cv2.imread('./img/neg/nature001.jpg')
-print(motorcycle_detector.predict(test_img))
-test_img = cv2.imread('./img/neg/nature003.jpg')
-print(motorcycle_detector.predict(test_img))
+def save_object(obj, filename):
+    with open(filename, 'wb') as output:
+        pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
 
-print('\nReal Test Images')
 
-print('\nPositive Tests')
-test_images_path = detector.directory_filenames('./img/test_pos')
-for img_path in test_images_path:
-    img = cv2.imread(img_path)
-    print(motorcycle_detector.predict(img), ' ', img_path)
+def get_object(filename):
+    with open(filename, 'rb') as input_:
+        ans = pickle.load(input_)
+    return ans
 
-print('\nNegative Tests')
-test_images_path = detector.directory_filenames('./img/test_neg')
-for img_path in test_images_path:
-    img = cv2.imread(img_path)
-    print(motorcycle_detector.predict(img))
+
+def detect_all(object_detector, test_neg_dir='./img/test_neg', test_pos_dir='./img/test_pos', to_print=True):
+    """
+    Uses the detector to identify all the test images. Then calculates (precision, recall)
+    :param object_detector: A detector that implements the `predict` method for images
+    :param test_neg_dir: A directory with images that are supposed to return False
+    :param test_pos_dir: A directory with images that are supposed to return True
+    :return: A tuple (precision, recall) describing the results of the detection
+    """
+
+    def print_(*args):
+        if to_print:
+            for arg in args:
+                print(arg, end='')
+            print()
+
+    print_('\nPositive Tests')
+    test_images_path = detector.directory_filenames(test_pos_dir)
+    n_pos_samples = len(test_images_path)
+    true_positive_count = 0
+    for img_path in test_images_path:
+        img = cv2.imread(img_path)
+        prediction = object_detector.predict(img)
+        if prediction:
+            true_positive_count += 1
+        if to_print:
+            print(prediction, ' ', img_path)
+
+    print_('\nNegative Tests')
+    test_images_path = detector.directory_filenames(test_neg_dir)
+    false_positive_count = 0
+    for img_path in test_images_path:
+        img = cv2.imread(img_path)
+        prediction = object_detector.predict(img)
+        if prediction:
+            false_positive_count += 1
+        print_(prediction, ' ', img_path)
+
+    precision = float(true_positive_count) / (true_positive_count + false_positive_count)
+    recall = float(true_positive_count) / n_pos_samples
+    return precision, recall
+
+
+def create_detector(filename='motorcycleDetector.pkl', to_train=False, to_save=False, train_neg_dir='./img/neg',
+                    train_pos_dir='./img/pos'):
+    if to_train:
+        motorcycle_detector = detector.ObjectDetector()
+        motorcycle_detector.train(train_neg_dir, train_pos_dir)
+        if to_save:
+            save_object(motorcycle_detector, filename)
+        return motorcycle_detector
+    else:
+        return get_object(filename)
+
+
+def create_roc():
+    points = []
+    for k in range(2, 7):
+        detector.ObjectDetector.PARAMS['FEATURES_NUM'] = k
+        motorcycle_detector = create_detector(to_train=True, to_save=False)
+        precision, recall = detect_all(motorcycle_detector)
+        print('Precision, Recall:', precision, recall)
+        points.append((precision, recall))
+    x = np.array([p[0] for p in points])
+    y = np.array([p[1] for p in points])
+    plt.plot(x, y)
+    plt.axis([0, 1, 0, 1])
+    plt.xlabel('Precision')
+    plt.ylabel('Recall')
+    plt.title('Precision/Recall as Number of Features Change')
+    plt.grid(True)
+    plt.show()
+
+
+detect_all(create_detector(to_train=True, to_save=True), to_print=True)
--- BagOfWordsDetector/detector.py
+++ BagOfWordsDetector/detector.py
@@ -1,9 +1,15 @@
 import os
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
 from sklearn.svm import SVC
 import numpy as np
 
 import cv2
+
+HEIGHT, WIDTH = 0, 1
+KEYPOINT_X, KEYPOINT_Y = 0, 1
+NOT_OBJECT, OBJECT = -1, 1
 
 
 def directory_filenames(dir_name):
@@ -13,19 +19,43 @@ def directory_filenames(dir_name):
     return ans
 
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+def display_points(img, points, color=(0, 255, 0), size=5, window_name='Points Display'):
+    img_copy = np.copy(img)
+    for point in points:
+        print((int(point.x), int(point.y)))
+        cv2.circle(img_copy, (int(point.x), int(point.y)), radius=size, color=color, thickness=3)
+        cv2.imshow(window_name, img_copy)
+        cv2.waitKey(5)
+    cv2.imshow(window_name, img_copy)
+
+def show_siftpoints(img):
+    sifter = cv2.xfeatures2d.SIFT_create()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    key_points = sifter.detect(gray, None)
+    img_points = np.copy(img)
+    cv2.drawKeypoints(img, key_points, img_points)
+    cv2.imshow('Siftpoints', img_points)
+
 class ObjectDetector:
     PARAMS = {
-        'FEATURES_NUM': 20
+        'FEATURES_NUM': 10
     }
 
-    def __init__(self):
+    def __init__(self, to_print=True):
         self._k_features = None
         self._classifier = None
+        self.to_print = to_print
 
     @staticmethod
     def _closest_point(goal, points):
         points = np.asarray(points)
-        dist_2 = np.sum((points - goal) ** 2, axis=1)
+        dist_2 = np.sum(np.square((points - goal)), axis=1)
         return np.argmin(dist_2)
 
     @staticmethod
@@ -37,27 +67,46 @@ class ObjectDetector:
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         sifter = cv2.xfeatures2d.SIFT_create()
         key_points = sifter.detect(gray, None)
-        width = gray.shape[0]
-        height = gray.shape[1]
+        width = float(gray.shape[WIDTH])
+        height = float(gray.shape[HEIGHT])
+
         estimated_size_factor = (width + height) / 2
-        # TODO - calculate the size factor specificly for each key point depending on the <angle, width, height>
-        return [(p.angle, p.pt[0] / height, p.pt[1] / width, p.size / estimated_size_factor) for p in key_points]
+        # TODO - calculate the size factor specifically for each key point depending on the <angle, width, height>
+        # return [(p.angle, p.pt[0] / height, p.pt[1] / width, p.size / estimated_size_factor) for p in key_points]
+        return [(p.pt[KEYPOINT_Y] / height, p.pt[KEYPOINT_X] / width) for p in key_points]
+
+    def print(self, *args):
+        if self.to_print:
+            for arg in args:
+                print(arg, end='')
+            print()
 
     def train_classifier(self, negative_vectors, positive_vectors):
         all_vectors = []
         all_vectors.extend(negative_vectors)
         all_vectors.extend(positive_vectors)
         # create an array of expected results that looks like this: [-1, -1, -1, -1 ..., 1, 1, 1, 1]
-        expected_results = np.where(np.arange(start=1, stop=len(all_vectors) + 1) <= len(negative_vectors), -1, 1)
+        expected_results = np.where(np.arange(start=1, stop=len(all_vectors) + 1) <= len(negative_vectors), NOT_OBJECT,
+                                    OBJECT)
 
-        self._classifier = SVC().fit(all_vectors, expected_results)
+        self._classifier = KNeighborsClassifier(8).fit(all_vectors, expected_results)
+        # self._classifier = RandomForestClassifier().fit(all_vectors, expected_results)
 
     def set_k_features(self, positive_keypoints):
         samples = np.array(positive_keypoints, dtype=np.float32)
 
-        criteria = (cv2.TERM_CRITERIA_MAX_ITER, 20, 1.)
-        _, _, self._k_features = cv2.kmeans(samples, ObjectDetector.PARAMS['FEATURES_NUM'], None, criteria, 10,
+        iterations = 20
+        repetitions = 10
+        criteria = (cv2.TERM_CRITERIA_MAX_ITER, iterations, 1.)
+        _, _, self._k_features = cv2.kmeans(samples, ObjectDetector.PARAMS['FEATURES_NUM'], None, criteria, repetitions,
                                             cv2.KMEANS_RANDOM_CENTERS)
+
+    def display_k_features(self, img):
+        height = float(img.shape[HEIGHT])
+        width = float(img.shape[WIDTH])
+        self.print('K features:', self._k_features)
+        points = [Point(p[WIDTH] * width, p[HEIGHT] * height) for p in self._k_features]
+        display_points(img, points)
 
     def feature_vector(self, img):
         if self._k_features is None:
@@ -79,7 +128,7 @@ class ObjectDetector:
             img = cv2.imread(img_path)
             positive_keypoints.extend(ObjectDetector._img_keypoint_vectors(img))
 
-            print('Calculating features... {:.0f}%'.format(100. * i / len(positive_images_paths)))
+            self.print('Calculating features... {:.0f}%'.format(100. * i / len(positive_images_paths)))
             i += 1
 
         # Cluster all the keypoints to k features
@@ -93,7 +142,7 @@ class ObjectDetector:
             img = cv2.imread(img_path)
             positive_feature_vectors.append(self.feature_vector(img))
 
-            print('Calculating feature vectors for positive points... {:.0f}%'.format(
+            self.print('Calculating feature vectors for positive points... {:.0f}%'.format(
                 100. * i / len(positive_images_paths)))
             i += 1
         i = 1
@@ -101,7 +150,7 @@ class ObjectDetector:
             img = cv2.imread(img_path)
             negative_feature_vectors.append(self.feature_vector(img))
 
-            print('Calculating feature vectors for negative points... {:.0f}%'.format(
+            self.print('Calculating feature vectors for negative points... {:.0f}%'.format(
                 100. * i / len(negative_images_paths)))
             i += 1
 
@@ -113,6 +162,10 @@ class ObjectDetector:
             raise Exception('Object detector cannot predict before being trained')
         else:
             unknown = np.stack((self.feature_vector(img),))
-            return self._classifier.predict(unknown)[0] == 1
-
-
+            if self._classifier.predict(unknown)[0] == OBJECT:
+                return True
+            img = np.fliplr(img)
+            unknown = np.stack((self.feature_vector(img),))
+            if self._classifier.predict(unknown)[0] == OBJECT:
+                return True
+            return False
